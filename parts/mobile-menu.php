dacf0da80d8644670d4b74023b6bf844d4797c71
Craig Fowler <craig@wearegiants.com>
2016-02-12 11:50:16 -0800

    mobile menu

--- parts/mobile-menu.php
+++ parts/mobile-menu.php
@@ -1,7 +1,25 @@
 <div id="mobile-menus">
 <div class="fs-row">
 <div id="mobile-menu_container" class="fs-cell fs-all-full">
+<div class="fs-row">
+
+<div class="fs-cell fs-lg-8 fs-md-4 fs-sm-2">
 <?php echo strip_tags(wp_nav_menu( $mainMenu ), '<a>' ); ?>        
+</div>
+
+<div id="mobile-menus__small" class="fs-cell fs-lg-4 fs-md-2 fs-sm-1">
+<br>
+<a href="/http://lfla.org/" target="blank" class="btn btn-nav">LFLA.org</a>
+<a href="/http://lfla.org/about/" target="blank" class="btn btn-nav">About</a>
+<a href="/http://lfla.org/what-we-fund/" target="blank" class="btn btn-nav">What We Fund</a>
+<a href="/http://lfla.org/support-us/" target="blank" class="btn btn-nav">Support Us</a>
+<a href="/http://lfla.org/aloud" target="blank" class="btn btn-nav">Aloud</a>
+<a href="/http://lfla.org/calendar" target="blank" class="btn btn-nav">Calendar</a>
+<a href="/http://shop.lfla.org" target="blank" class="btn btn-nav">The Library Store</a>
+<a href="/http://lfla.org/blog/" target="blank" class="btn btn-nav">Blog</a>
+</div>
+
+</div>
 </div>
 </div>
 </div>
